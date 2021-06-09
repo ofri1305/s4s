2cd3265d695b3607def06a0fd05c602104f9c8b1
Jeffrey <jeffreybenabou@gmail.com>
2021-06-09 18:03:12 +0300

    Add Material Recycler

--- app/src/main/java/com/example/try2/FragmentMaterial.java
+++ app/src/main/java/com/example/try2/FragmentMaterial.java
@@ -13,19 +13,33 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.try2.objects.Chat;
+import com.example.try2.objects.Material;
+import com.example.try2.recyclers.ChatRecycler;
+import com.example.try2.recyclers.MaterialRecycler;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 import javax.annotation.Nullable;
 
 public class FragmentMaterial extends Fragment {
     StorageReference storageReference;
-
-
+    FirebaseStorage storage ;
+    FirebaseFirestore fStore;
+    private String nameOfCourse;
+    private ArrayList<Material>materials;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,12 +50,27 @@ public class FragmentMaterial extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @androidx.annotation.Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+         storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
+        fStore= FirebaseFirestore.getInstance();
+         nameOfCourse=getArguments().getString("nameOfCourse");
         Button button=getView().findViewById(R.id.upload_file);
-
+        loadAllMaterials();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 uploadFile();
+
+            }
+        });
+    }
+
+    private void loadAllMaterials() {
+        fStore.collection(nameOfCourse).document("material").collection("materialsObjects").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                materials= (ArrayList<Material>) task.getResult().toObjects(Material.class);
+                setRecyclerView();
 
             }
         });
@@ -66,16 +95,27 @@ public class FragmentMaterial extends Fragment {
         }
     }
     private void uploadImageToFirebase(Uri imageUri) {
-        String nameOfCourse=getArguments().getString("nameOfCourse");
+
         Long date=new Date().getTime();
-        Log.i("nameOfCourse", "uploadImageToFirebase: "+nameOfCourse);
-        StorageReference fileRef = storageReference.child("nameOfCourse"+"/material/"+"date");
+        StorageReference fileRef = storageReference.child(nameOfCourse+"/material/"+date);
         fileRef.putFile(imageUri).addOnSuccessListener((OnSuccessListener)(taskSnapshot)->{
             fileRef.getDownloadUrl().addOnSuccessListener((OnSuccessListener)(uri)->{
+                Material material=new Material(uri.toString(),"ttt",date.toString());
+                fStore.collection(nameOfCourse).document("material").collection("materialsObjects").document(date.toString()).set(material);
+                materials.add(material);
+                setRecyclerView();
               Toast.makeText(getContext(), "ffffff", Toast.LENGTH_LONG).show();
             });
         }).addOnFailureListener((e) -> {
 
         });
+    }
+
+    private void setRecyclerView() {
+        RecyclerView recyclerView = getView().findViewById(R.id.material_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+        MaterialRecycler recycler= new MaterialRecycler(materials,nameOfCourse,getContext());
+        recyclerView.setAdapter(recycler);
     }
 }
