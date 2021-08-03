8e4f2b67c7e80c6ccd9eb2db0e4b3abfcbf24874
ofri1305 <ofri2377@gmail.com>
2021-08-03 14:32:28 +0300

    half log out report

--- app/src/main/java/com/example/try2/mainPageFragments/HomeFragment.java
+++ app/src/main/java/com/example/try2/mainPageFragments/HomeFragment.java
@@ -76,6 +76,7 @@ public class HomeFragment extends Fragment {
         }
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
